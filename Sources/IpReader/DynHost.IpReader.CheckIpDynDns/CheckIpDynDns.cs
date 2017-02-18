bdb708e76f30572bfea37a0630130db6f9a2c298
cjean <cyrille.jean@i-tech-net.com>
2017-02-18 02:23:16 +0100

    Add CheckIpDynDns IpReader and Unit Test

---
+++ Sources/IpReader/DynHost.IpReader.CheckIpDynDns/CheckIpDynDns.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DynHost.IpReader.CheckIpDynDns
+{
+    public class CheckIpDynDns
+    {
+        public CheckIpDynDns()
+        {
+        }
+
+        public string GetCurrentIp()
+        {
+            return "IP";
+        }
+    }
+}
---
+++ Tests/DynHostUpdateService.UnitTest/IpReader/CheckIpDynDnsTest.cs
@@ -0,0 +1,20 @@
+﻿using DynHost.IpReader.CheckIpDynDns;
+using NFluent;
+using NUnit.Framework;
+
+namespace DynHostUpdateService.UnitTest.IpReader
+{
+    /// <summary>
+    /// Summary description for DynHost
+    /// </summary>
+    [TestFixture]
+    public class CheckIpDynDnsTest
+    {
+        [Test]
+        public void CheckGetIpReturnNoEmptyString()
+        {
+            var ipReader = new CheckIpDynDns();
+            Check.That(ipReader.GetCurrentIp()).IsNotNull().And.IsNotEmpty();
+        }
+    }
+}
